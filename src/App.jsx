1218c554b27dc1c93fca4232d5daffcae831ed1f
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-07-15 16:32:15 -0400

    Adding fake authentication setting up context & Adding fake authentication implementing login

--- src/App.jsx
+++ src/App.jsx
@@ -12,43 +12,46 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { AuthProvider } from "./context/FakeAuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Logo Route */}
-          <Route path="/" element={<Homepage />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Logo Route */}
+            <Route path="/" element={<Homepage />} />
 
-          {/* Pricing Route */}
-          <Route path="pricing" element={<Pricing />} />
+            {/* Pricing Route */}
+            <Route path="pricing" element={<Pricing />} />
 
-          {/* Product Route */}
-          <Route path="product" element={<Product />} />
+            {/* Product Route */}
+            <Route path="product" element={<Product />} />
 
-          {/* Login Route */}
-          <Route path="login" element={<Login />} />
+            {/* Login Route */}
+            <Route path="login" element={<Login />} />
 
-          {/* Application Route */}
-          <Route path="app" element={<AppLayout />}>
-            {/* Application Nested Route (Index Cities) */}
-            <Route index element={<Navigate replace to="cities" />} />
+            {/* Application Route */}
+            <Route path="app" element={<AppLayout />}>
+              {/* Application Nested Route (Index Cities) */}
+              <Route index element={<Navigate replace to="cities" />} />
 
-            <Route path="cities/:id" element={<City />} />
+              <Route path="cities/:id" element={<City />} />
 
-            {/* Application Nested Route (Cities) */}
-            <Route path="cities" element={<CityList />} />
+              {/* Application Nested Route (Cities) */}
+              <Route path="cities" element={<CityList />} />
 
-            <Route path="countries" element={<CountryList />} />
+              <Route path="countries" element={<CountryList />} />
 
-            <Route path="form" element={<Form />} />
-          </Route>
+              <Route path="form" element={<Form />} />
+            </Route>
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
